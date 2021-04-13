09f09b76a0095f48ff7bdb3a196afc52ee895d8a
merkel1860 <merkelmller@yandex.ru>
2021-04-12 21:00:23 -0400

    Adding sql script sampleScript.sql for creating database schema on mysql instance. the DB name is esihdb and it is running by nebel. By now, each student enrolled via  enroll_student_form is automatically added to DB.

--- src/core/utils/Cxo.java
+++ src/core/utils/Cxo.java
@@ -2,23 +2,30 @@ package core.utils;
 
 import core.CourseLevel;
 import core.Degree;
+import core.Student;
+import org.apache.ibatis.jdbc.ScriptRunner;
 
+import java.io.*;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Cxo {
+public class Cxo   {
     private static Connection connection;
     private static ResultSet resultSet;
     private static String sqlQuery;
     private static Statement stm;
-    private static String cred = "jdbc:mysql://127.0.0.1:3306/esihdb";
+    private static String cred = "jdbc:mysql://127.0.0.1:3306/esihdb?serverTimezone=UTC";
+
+
+    public Cxo() {
+    }
 
     public static void pickBaseParameter(String param){
         sqlQuery ="select * from" + " "+param;
     }
-    public static void init() {
-
+    public static void initConnection() {
+        // loading driver
         try {
         // new Driver name : com.mysql.cj.jdbc.Driver
         // old Driver name : com.mysql.jdbc.Driver
@@ -28,6 +35,7 @@ public class Cxo {
             System.out.println(e.toString());
         }
 
+        // Establishing a new connection
         try {
             connection= DriverManager.
                     getConnection(cred,"nebel","nebel1984");
@@ -35,20 +43,53 @@ public class Cxo {
             e.printStackTrace();
         }
 
-        try {
-            stm= connection.createStatement();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+    }
 
+    public static void fechtFromDB() {
         try {
             resultSet = stm.executeQuery(sqlQuery);
         } catch (SQLException e) {
             e.printStackTrace();
         }
+    }
+
+    public static void createDBSchema(){
+        // Creating esihdb schema
+        try {
+            // There is no needs for a new statement
+            // because MyBatis wrapper is taking care of that part.
+            //stm= connection.createStatement();
+            System.out.println("Connection established......");
+            //Initialize the script runner
+            ScriptRunner sr = new ScriptRunner(connection);
+            //Creating a reader object
+            Reader reader = new BufferedReader(
+                    new FileReader("/home/schatz/IdeaProjects" +
+                            "/ESIH_App/src/db/sampleScript.sql"));
+            //Running the script
+            sr.runScript(reader);
+            System.out.println("Hello");
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
 
     }
 
+    public static void insertData(Student student){
+        try {
+            String sqlString = "call esihdb.addStudent(?,?,?,?)";
+            PreparedStatement pr = connection.prepareStatement(sqlString);
+            pr.setString(1,student.getFirstname());
+            pr.setString(2,student.getLastname());
+            pr.setString(3,student.getStudentGender());
+            pr.setInt(4,student.getYear());
+             pr.execute();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+
+        }
+    }
     public static List<Degree> fetchDegreeFromDB(){
         List<Degree> degreeList = new ArrayList<>();
         try {
@@ -65,6 +106,7 @@ public class Cxo {
         return degreeList;
     }
 
+/*
     public static List<CourseLevel> fetchCourseLevelFromDB(){
         List<CourseLevel> courseLevels = new ArrayList<>();
         try {
@@ -81,6 +123,7 @@ public class Cxo {
         }
         return courseLevels;
     }
+*/
 
 
 }
